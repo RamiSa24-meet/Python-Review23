26c54644a00241d0acd4d1872ef42c0f3e901f81
RamiSa24-meet <139688880+RamiSa24-meet@users.noreply.github.com>
2023-07-16 21:44:35 +0300

    mmm

---
+++ pythonReview.py
@@ -0,0 +1,44 @@
+def create_youtube_video(title,descreption):
+	dict = {'title':title,'descreption':descreption,'like':0,'dislike':0,"username":{}}
+
+	return dict 
+
+
+def like(dicionary):
+	if 'like' in dicionary:
+		dicionary['like'] = dicionary['like']+1
+	return dicionary
+
+
+
+def dislike(dicionary):
+	if 'dislike' in dicionary:
+		dicionary['dislike'] = dicionary['dislike']+1
+	return dicionary
+
+def add_comment(youtubename,username,comment_text):
+	youtubename['username'][username]=comment_text
+
+
+	return youtubename
+
+
+def check():
+	dict = create_youtube_video('lol','this is my youtube video')
+	like(dict)
+	dislike(dict)
+	print(dict['like'])
+	print(dict['dislike'])
+	add_comment(dict,'rami11','I love this video')
+	add_495(dict)
+	print(dict['like'])
+
+def add_495(dicionary):
+	for  x in range(495):
+		like(dicionary)
+
+
+
+
+
+check()
