f13c6a843c2160ac51f8d920693f1b34de90b38e
RamiSa24-meet <139688880+RamiSa24-meet@users.noreply.github.com>
2023-08-02 13:28:22 +0300

    m

--- pythonReview.py
+++ pythonReview.py
@@ -1,6 +1,7 @@
 def create_youtube_video(title,descreption):
-	dict = {'title':title,'descreption':descreption,'like':0,'dislike':0,"username":{}}
-
+	dict = {'title':title,'descreption':descreption,'like':0,'dislike':0,"username":{"rami":"wdjiawdijawdijwijdaw"},"hashtag":["","","","",""]}
+	for i in range(5):
+		dict["hashtag"][i]= input("enter a hashtag")
 	return dict 
 
 
@@ -8,6 +9,7 @@ def like(dicionary):
 	if 'like' in dicionary:
 		dicionary['like'] = dicionary['like']+1
 	return dicionary
+
 
 
 
@@ -23,12 +25,37 @@ def add_comment(youtubename,username,comment_text):
 	return youtubename
 
 
+
+
+def checksimilar(dict1,dict2):
+
+	for x in range(5):
+		c = 0 
+		for i in range:
+			if dict1["hashtag"][x] != dic2["hashtag"][i]:
+				break
+			else:
+				c= c+1
+		if c == 5 :
+			print(5)
+
+
+
+
+
+
+
+
+
+
+
 def check():
 	dict = create_youtube_video('lol','this is my youtube video')
 	like(dict)
 	dislike(dict)
 	print(dict['like'])
 	print(dict['dislike'])
+	print(dict['hashtag'])
 	add_comment(dict,'rami11','I love this video')
 	add_495(dict)
 	print(dict['like'])
